380eef37bed2070ffcf95b7f0e984788bb2970ec
Bruno Ribeiro <bbrunoo@icloud.com>
2023-12-12 19:37:51 -0300

    Parser for problem generation

--- trabalho_04/parser.sh
+++ trabalho_04/parser.sh
@@ -13,8 +13,8 @@ echo "(define (problem sudoku)" >> problem.hddl
 echo -e "\t(:domain sudoku)" >> problem.hddl
 echo -e "\t(:objects)" >> problem.hddl
 echo -e "\t(:init" >> problem.hddl
-echo -e "\t\t(inc r0 r1) (inc r1 r2) (inc r2 r3) (inc r3 r4) (inc r4 r5) (inc r5 r6) (inc r6 r7) (inc r7 r8)" >> problem.hddl
-echo -e "\t\t(inc c0 c1) (inc c1 c2) (inc c2 c3) (inc c3 c4) (inc c4 c5) (inc c5 c6) (inc c6 c7) (inc c7 c8)" >> problem.hddl
+#echo -e "\t\t(inc r0 r1) (inc r1 r2) (inc r2 r3) (inc r3 r4) (inc r4 r5) (inc r5 r6) (inc r6 r7) (inc r7 r8)" >> problem.hddl
+#echo -e "\t\t(inc c0 c1) (inc c1 c2) (inc c2 c3) (inc c3 c4) (inc c4 c5) (inc c5 c6) (inc c6 c7) (inc c7 c8)" >> problem.hddl
 for ((i=0; i<9; i++)); do
 	for ((j=0; j<9; j++)); do
 		box=$(get_box $i $j)
